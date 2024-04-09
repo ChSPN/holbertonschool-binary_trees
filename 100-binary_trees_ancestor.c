b74a41ec6fac105010c3c4e0c12ebda2a908a9c3
ChSPN <charlene.scomparin@gmail.com>
2024-04-09 16:26:01 +0200

    Write a function that finds the lowest common ancestor of two nodes

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,46 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ * @first: Pointer to the first node
+ * @second: Pointer to the second node
+ *
+ * Return: Pointer to the lowest common ancestor node, or NULL if not found
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+										const binary_tree_t *second)
+{
+	const binary_tree_t *ancestor;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	ancestor = first;
+	while (ancestor != NULL)
+	{
+		if (binary_tree_is_descendant_of(second, ancestor))
+			return ((binary_tree_t *)ancestor);
+		ancestor = ancestor->parent;
+	}
+
+	return (NULL);
+}
+
+/**
+ * binary_tree_is_descendant_of - Checks if a node is a descendant of another
+ * @node: Pointer to the potential descendant node
+ * @ancestor: Pointer to the potential ancestor node
+ *
+ * Return: 1 if @node is a descendant of @ancestor, 0 otherwise
+ */
+int binary_tree_is_descendant_of(const binary_tree_t *node,
+									const binary_tree_t *ancestor)
+{
+	while (node != NULL)
+	{
+		if (node == ancestor)
+			return (1);
+		node = node->parent;
+	}
+	return (0);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -77,6 +77,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);/*task 18*/
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 
+int binary_tree_is_descendant_of(const binary_tree_t *node, const binary_tree_t *ancestor);
+
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 int binary_tree_is_complete(const binary_tree_t *tree);/*task 21*/
