4ad32f2699ca5c19505615919c735b191aeb1576
ChSPN <charlene.scomparin@gmail.com>
2024-04-09 15:16:05 +0200

    Write a function that measures the size of a binary tree (betty style)

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,13 +8,13 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t size = 0;
+	size_t size = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    /* Recursively count the size of the left and right subtrees */
-    size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+	/* Recursively count the size of the left and right subtrees */
+	size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
 
-    return (size);
+	return (size);
 }
