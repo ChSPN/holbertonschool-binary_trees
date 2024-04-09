5cd51245f20fb8ef9cb7ed1ea119a79955b9bfac
ChSPN <charlene.scomparin@gmail.com>
2024-04-09 14:33:57 +0200

    Write a function that creates a binary tree node (betty style)

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,26 +2,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Create a binary tree node
- * @parent: Pointer to the parent node of the node to create
- * @value: Value to put in the new node
+ * binary_tree_node - Crée un nœud d'arbre binaire
+ * @parent: Pointeur vers le nœud parent du nœud à créer
+ * @value: Valeur à mettre dans le nouveau nœud
  *
- * Return: Pointer to the new node, or NULL on failure
+ * Return: Pointeur vers le nouveau nœud, ou NULL en cas d'échec
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    /* Allocation de mémoire pour le nouveau nœud */
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* Allocation de mémoire pour le nouveau nœud */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    /* Initialisation des valeurs du nouveau nœud */
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	/* Initialisation des valeurs du nouveau nœud */
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    return (new_node);
+	return (new_node);
 }
